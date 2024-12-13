dcca5c4faea84a0a8e0cfed4f3eb64824361391e
samuel ibitoye <ibitoyesamuel25@gmail.com>
2024-12-13 12:18:03 +0100

    Funfact

--- main/app/(site)/page.tsx
+++ main/app/(site)/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
       <Feature />
       <About />
       <FeaturesTab />
-      <FunFact />
+      {/* <FunFact /> */}
       <Integration />
       <CTA />
       <FAQ />
