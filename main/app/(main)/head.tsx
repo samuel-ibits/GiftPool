fd0d7a3aab0fed0d45cd8855329c6c556e683346
samuel ibitoye <ibitoyesamuel25@gmail.com>
2025-05-30 15:33:07 +0100

    Restructure App

---
+++ main/app/(main)/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title> GiftPool| Simplify Giveaways & Crowdfunding</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Simplify Giveaways & Crowdfunding" />
+      <link rel="icon" href="/images/favicon.ico" />
+    </>
+  );
+}
