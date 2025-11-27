8f6c7f4897f4ebcb0526521b4acd25abccd503b9
samuel ibitoye <ibitoyesamuel25@gmail.com>
2025-11-27 15:07:34 +0100

    Update meta

--- main/app/(auth)/signup/page.tsx
+++ main/app/(auth)/signup/page.tsx
@@ -2,8 +2,8 @@ import Signup from "@/components/Auth/Signup";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Sign Up Page - Solid SaaS Boilerplate",
-  description: "This is Sign Up page for Startup Pro",
+  title: "Giftpool | Sign Up",
+  description: "The Sign Up page for Giftpool application.",
   // other metadata
 };
 
